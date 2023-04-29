e43d37c01a428455d6e15865552f57e89f557a4f
kaia <89624646+kaiakm@users.noreply.github.com>
2023-04-28 21:52:31 -0500

    HELP I CAN'T FIX THE GRID

--- script.js
+++ script.js
@@ -21,10 +21,41 @@ cells.forEach(cell => {
     });
 });
 
-const btn = document.querySelector('.clear');
+const clearBtn = document.querySelector('.clear');
 
-btn.addEventListener('click', () => {
+clearBtn.addEventListener('click', () => {
     cells.forEach(cell => {
         cell.style.cssText = 'background: white';
     });
+});
+
+const newBtn = document.querySelector('.new');
+
+function createGrid(size) {
+    container.innerHTML = '';
+    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+    for (let i = 0; i < size * size; i++) {
+        const cell = document.createElement('div');
+        cell.classList.add('cell');
+        cell.style.cssText = 'background: white';
+        cell.addEventListener('mouseenter', () => {
+            cell.style.cssText = 'background: gray';
+        });
+
+        container.appendChild(cell);
+    };
+};
+
+newBtn.addEventListener('click', () => {
+    const gridSize = prompt('grid size (30 max):');
+
+    if (gridSize <= 30 && gridSize > 0) {
+        createGrid(gridSize);
+    } else if (gridSize === null) {
+
+    } else {
+        alert('a number between 1 and 30 thank you');
+    };
 });
