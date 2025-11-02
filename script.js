d7ca9e480dac1fabff318feefb93aa6e19b82a87
kaiakm <89624646+kaiakm@users.noreply.github.com>
2025-11-02 10:07:36 -0600

    created div in js

---
+++ script.js
@@ -0,0 +1,4 @@
+const container = document.querySelector('#container');
+const gridSquare = document.createElement('div');
+gridSquare.setAttribute('id', 'grid-square');
+container.appendChild(gridSquare);
