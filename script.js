25c060a2d67074701f492ac179d4b026812c6709
kaiakm <89624646+kaiakm@users.noreply.github.com>
2025-11-16 15:02:18 -0600

    added resetting function

--- script.js
+++ script.js
@@ -1,24 +1,13 @@
 const container = document.querySelector('#container');
 const containerWidth = container.clientWidth;
-
-// for (let i = 0; i < 16 * 16; i++) {
-//     const item = document.createElement("div");
-//     item.id = "grid-item";
-//     item.style.width = `${containerWidth / 16}px`;
-//     item.style.height = `${containerWidth / 16}px`;
-//     item.addEventListener("mouseenter", (e) => {
-//         e.target.style.backgroundColor = "black";
-//     });
-
-//     container.appendChild(item);
-// };
+const body = document.querySelector("body");
 
 function createGrid (size) {
     for (let i = 0; i < size * size; i++) {
         const item = document.createElement("div");
         item.id = "grid-item";
-        item.style.width = `${containerWidth / 16}px`;
-        item.style.height = `${containerWidth / 16}px`;
+        item.style.width = `${containerWidth / size}px`;
+        item.style.height = `${containerWidth / size}px`;
         item.addEventListener("mouseenter", (e) => {
             e.target.style.backgroundColor = "black";
         });
@@ -27,8 +16,16 @@ function createGrid (size) {
     };
 }
 
-
 const gridSize = document.querySelector('#grid-size');
+const resetGrid = document.querySelector("#reset-grid");
+
+resetGrid.addEventListener("click", () => {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+    createGrid(16);
+})
+
 gridSize.addEventListener("click", () => {
     let input = prompt(`Enter a number (1-100)`);
 
@@ -38,9 +35,11 @@ gridSize.addEventListener("click", () => {
         } else {
             alert(`Invalid. Please try again.`);
         }
-    }
+    };
 
+    eraseCurrentGrid();
     checkValidNumber();
+    createGrid();
 });
 
 createGrid(16);
