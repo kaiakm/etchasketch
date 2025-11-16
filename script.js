2b6bc7674eec7eb7e7a1ab0c1284ffc75e65883f
kaiakm <89624646+kaiakm@users.noreply.github.com>
2025-11-16 10:42:30 -0600

    added basic code

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 const container = document.querySelector('#container');
-const gridSquare = document.createElement('div');
-gridSquare.setAttribute('id', 'grid-square');
-container.appendChild(gridSquare);
+
+const numRows = 16;
+const numCols = 16;
+
+for (let i = 0; i < numRows; i++) {
+    for (let j = 0; j < numCols; j++) {
+        const gridSquare = document.createElement('div');
+        gridSquare.setAttribute('id', 'grid-square');
+        container.appendChild(gridSquare);
+    }
+}
